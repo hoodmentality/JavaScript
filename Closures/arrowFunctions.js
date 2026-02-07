bbb92f9036bb512e022eea0218dfe4e54301d960
Error_404 <built.different7813@gmail.com>
2026-02-07 22:48:14 +0530

    Add files via upload

---
+++ Closures/arrowFunctions.js
@@ -0,0 +1,25 @@
+// Example - 1
+var multiply = function(x,y) {
+    return x*y;
+}
+
+console.log(multiply(4,6));
+
+
+// Example - 2
+var multiply = (x,y) => {
+    return x*y;
+};
+
+console.log(multiply(5,5));
+
+
+// Example - 3
+var multiply = (x,y) => x*y;
+
+console.log(multiply(5,6));
+
+// Example - 4
+var double = x => 2*x;
+
+console.log(double(5));
